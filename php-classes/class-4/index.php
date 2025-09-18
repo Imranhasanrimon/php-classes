97b5291fd7c87fe2986b08a1b5a49802ec7cef1a
Imran Hasan <imranhasanrimon5@gmail.com>
2025-09-18 17:51:10 +0600

    learning while loop and implementing this in dynamic html

--- php-classes/class-4/index.php
+++ php-classes/class-4/index.php
@@ -1,9 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#">
+        Year
+        <select name="2000" id="">
+            <?php
+            $year = 2000;
+            while ($year<=2050){
+                ?>
 
+                <option value="<?php echo $year?>"><?php echo $year ?></option>
 
-//spaceship operator
-
-echo 2<=>2
-
-
-?>
+            <?php
+                $year ++;
+            }
+            ?>
+            
+        </select>
+    </form>
+</body>
+</html>
