6225063f1c7bc4577d389b41a059047228413069
Imran Hasan <imranhasanrimon5@gmail.com>
2025-09-21 10:57:33 +0600

    cleaning the code base

--- php-classes/class-4/index.php
+++ php-classes/class-4/index.php
@@ -1,42 +1,7 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="#">
-        Year
-        <select name="2000" id="">
-            <?php
-            $year = 2000;
-            while ($year <= 2050) {
-            ?>
 
-                <option value="<?php echo $year ?>"><?php echo $year ?></option>
-
-            <?php
-                $year++;
-            }
-            ?>
-            
-        </select>
-    </form>
-</body>
-</html> -->
 
 <?php
 
 
 
-for ($iterate = 1; $iterate <= 10; $iterate++) {
-?>
-
-    <li> <?php echo "Imran $iterate" ?></li>
-<?php
-    if ($iterate == 2) {
-        break;
-    }
-}
 ?>
